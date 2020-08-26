60c7b94776efe01d1fd3c653a10e915db7558fd0
Wuyiwai <992040155@qq.com>
2020-08-27 01:18:21 +0800

    feat: 创建型-抽象工厂模式

---
+++ Creational/AbstractFactory/DigitalProduct.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace DesignPatterns\Creational\AbstractFactory;
+
+class DigitalProduct implements Product
+{
+    private $price;
+    public function __construct(int $price)
+    {
+        $this->price = $price;
+    }
+
+    public function calculatePrice(): int
+    {
+        return $this->price;
+    }
+}
